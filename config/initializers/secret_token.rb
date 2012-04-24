be01a6267c97377556b9125ace83ccfae6a102ed
Jonathan Julian <jonathan.m.julian@gmail.com>
2012-04-24 14:48:02 -0500

    First commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LetsBuildABackboneApp::Application.config.secret_token = 'c4e7e602df90862f91d2ad48ba084e5c98a818cd31311b5688fe1872d0bb66fd8a3626d0f4567499d6cc1a096776b3d95c0003ec74777124a5c1b62064c09330'
